fe6e44be7b4f67c913ae89b62f2f63990a7bdd97
Carlos Rivera <crivera8681@gmail.com>
2021-04-29 23:28:24 -0700

    Add project files.

---
+++ Models/Bug.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BugTrackerWebApp.Models
+{
+    public class Bug
+    {
+        public int Id { get; set; }
+
+        public string BugType { get; set; }
+
+        public string BugDescription { get; set; }
+
+        public Bug()
+        {
+
+        }
+    }
+}
---
+++ Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(BugTrackerWebApp.Startup))]
+namespace BugTrackerWebApp
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
